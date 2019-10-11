04df3432abd77a3ba7fa842cf5ad7429dc967226
Ben van Enckevort <github@benvan.co.uk>
2019-10-11 17:00:32 +0100

    feat: added build config for umd

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var config = {
+  entry: './src/index',
+  module: {
+    rules: [
+      { test: /\.js$/, use: [ 'babel-loader' ], exclude: /node_modules/ }
+    ]
+  },
+  output: {
+    library: 'GooseModule',
+    libraryTarget: 'umd'
+  },
+
+}
+
+// if (process.env.NODE_ENV === 'production') {
+//   config.optimization = {
+//     minimize: true
+//   }
+// }
+
+module.exports = config
+
