eb2d5955a55b084d250d82026b1aaa9985d21a29
Ben van Enckevort <github@benvan.co.uk>
2019-10-11 17:36:27 +0100

    feat: smaller bundle size

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
+var webpack = require('webpack')
 var config = {
   entry: './src/index',
+	mode: 'production',
   module: {
     rules: [
       { test: /\.js$/, use: [ 'babel-loader' ], exclude: /node_modules/ }
@@ -9,14 +11,14 @@ var config = {
     library: 'GooseModule',
     libraryTarget: 'umd'
   },
-
+	externals:['react','redux','react-redux','redux-saga','fn-update']
 }
 
-// if (process.env.NODE_ENV === 'production') {
-//   config.optimization = {
-//     minimize: true
-//   }
-// }
+ //if (process.env.NODE_ENV === 'production') {
+	 //config.optimization = {
+		 //minimize: true
+	 //}
+ //}
 
 module.exports = config
 
